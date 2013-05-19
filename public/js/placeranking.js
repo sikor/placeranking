6a6ade158cadd32797f48b26f0e40bd93ee1ac7c
sikor <pasikora@student.agh.edu.pl>
2013-05-19 17:13:17 +0200

    Added Markers color.

--- public/js/placeranking.js
+++ public/js/placeranking.js
@@ -8,26 +8,71 @@ function initialize() {
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 
+    var markerVisualization = function(opinion, position){
+         var marker = new google.maps.Marker({
+                            position: position,
+                            map: map,
+                            title: opinion.comment
+          });
+
+        return marker
+    }
+
+    var styleMarkerVisualization = function(opinion, position){
+        var color = opinion.isPositive =="True" ? "#00FF00" : "#FF0000"
+        var marker = new StyledMarker({
+            styleIcon:new StyledIcon(
+                StyledIconTypes.MARKER,
+                {
+                color:color
+                }
+            ),
+            position:position,
+            map:map
+        });
+        return marker
+    }
+    var circleVisualization = function(opinion, position){
+        var opinionOptions = {
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FF0000',
+          fillOpacity: 0.35,
+          map: map,
+          center: position,
+          radius: 30
+        };
+        var opinionCircle = new google.maps.Circle(opinionOptions);
+        return opinionCircle
+    }
+
+    var infoWindowListener = function(opinion, marker){
+        var infowindow = new google.maps.InfoWindow();
+        google.maps.event.addListener(marker, 'click', function(){
+            infowindow.setContent(opinion.comment);
+            infowindow.open(map, marker)
+        })
+    }
+
+
+
+    var visualizationEngine = styleMarkerVisualization
+    var listenerEngine = infoWindowListener
+
+
     $.ajax({
         url: "/rest/opinion",
-        cache: false,
+        cache: true,
         dataType: "json"
     }).done( function(jsonOpinions){
             $.each(jsonOpinions, function(index, opinion){
                 var latlngArr = opinion.location.split(",");
                 var position = new google.maps.LatLng(latlngArr[0], latlngArr[1])
-                var marker = new google.maps.Marker({
-                            position: position,
-                            map: map,
-                            title: opinion.comment
-                     });
-                var infowindow = new google.maps.InfoWindow();
-                google.maps.event.addListener(marker, 'click', function(){
-                    infowindow.setContent(opinion.comment);
-                    infowindow.open(map, marker)
-                })
+                var obj = visualizationEngine(opinion, position)
+                listenerEngine(opinion, obj)
             })
-        });
+     });
 
 
     var formMarker = new google.maps.Marker({
