0e8f977a2b2ddfc19e30c5aa0483631a8d836670
sikor <pasikora@student.agh.edu.pl>
2013-06-03 16:24:13 +0200

    Sentimenter added.

--- placeranking/sentimenter.py
+++ placeranking/sentimenter.py
@@ -5,23 +5,22 @@ import urllib
 import urllib2
 import logging
 
-class Sentimenter:
 
-    def getSentiment(self, opinion):
+def getSentiment(opinion):
 
-        url = "http://text-processing.com/api/sentiment/"
-        data = {
-            "text": opinion
-        }
+    url = "http://text-processing.com/api/sentiment/"
+    data = {
+        "text": opinion
+    }
 
-        data = urllib.urlencode(data)
-        header = {
-            # "X-Mashape-Authorization": "1y2AiayLrMcGIq22X5jbBijhEPWogUhz"
-        }
-        request = urllib2.Request(url, data, header)
-        ans = json.load(urllib2.urlopen(request))
-        probability = ans["probability"]
-        log = ", ".join(("sentiment:", str(ans["label"]), str(probability["pos"]), str(probability["neg"]), str(probability["neutral"])))
-        logging.error(log)
+    data = urllib.urlencode(data)
+    header = {
+        # "X-Mashape-Authorization": "1y2AiayLrMcGIq22X5jbBijhEPWogUhz"
+    }
+    request = urllib2.Request(url, data, header)
+    ans = json.load(urllib2.urlopen(request))
+    probability = ans["probability"]
+    log = ", ".join(("sentiment:", str(ans["label"]), str(probability["pos"]), str(probability["neg"]), str(probability["neutral"])))
+    logging.error(log)
 
-        return ans["label"], probability["pos"], probability["neg"], probability["neutral"]
+    return str(ans["label"]), probability["pos"], probability["neg"], probability["neutral"]
